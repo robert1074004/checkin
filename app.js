b6bd04ed7e541c96c3295d9b180082dc0a4c7805
robert1074004 <abc50113@yahoo.com.tw>
2023-04-13 22:27:54 +0800

    feat: add passport and add user-controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
 const routes = require('./routes')
+const passport = require('./config/passport')
 const app = express()
 const PORT = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -11,6 +12,8 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
 const router = express.Router()
-
+const passport = require('passport')
 const checkinController = require('../controllers/checkin-controller')
+const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 router.get('/checkin', checkinController.getCheckin)
 router.get('/', (req, res) => res.redirect('/checkin'))
 router.use('/', generalErrorHandler)
