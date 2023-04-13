8e74139b674003fb5a1f04a27be8d602dac954c2
robert1074004 <abc50113@yahoo.com.tw>
2023-04-13 16:17:41 +0800

    feat: add check nodel

---
+++ migrations/20230413081113-create-check.js
@@ -0,0 +1,30 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Checks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      last_check: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Checks')
+  }
+}
