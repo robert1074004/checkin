82fa09c5ba7aebe5e2f7d606c7aba9b09263bf3d
robert1074004 <abc50113@yahoo.com.tw>
2023-04-13 21:01:42 +0800

    feat: add flash message and set session

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(methodOverride('_method'))
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 const checkinController = require('../controllers/checkin-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.get('/checkin', checkinController.getCheckin)
-router.use('/', (req, res) => res.redirect('/checkin'))
+router.get('/', (req, res) => res.redirect('/checkin'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
