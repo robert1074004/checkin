d8f49ac4de6784dc42591dc291424ee875c41afd
robert1074004 <abc50113@yahoo.com.tw>
2023-04-16 09:06:06 +0800

    feat: add setting page and modify signin page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,9 @@ const userController = {
   signInPage: (req, res) => {
     res.render('signin')
   },
+  settingPage: (req, res) => {
+    res.render('setting')
+  },
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入!')
     res.redirect('/checkin')
