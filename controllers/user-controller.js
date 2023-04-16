1cf98644faa3446060f7e5278c8a7ff4db1325c9
robert1074004 <abc50113@yahoo.com.tw>
2023-04-16 09:34:54 +0800

    feat: add setting function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,22 @@
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
 const userController = {
   signInPage: (req, res) => {
     res.render('signin')
   },
   settingPage: (req, res) => {
     res.render('setting')
+  },
+  setting: (req, res) => {
+    const password = req.body.password
+    return bcrypt.genSalt(10).then(salt => {
+      return Promise.all([bcrypt.hash(password, salt), User.findByPk(req.user.id)])
+    }).then(([hash, user]) => {
+      return user.update({ password: hash })
+    }).then(() => {
+      req.flash('success_messages', '修改密碼成功')
+      res.redirect('back')
+    })
   },
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入!')
