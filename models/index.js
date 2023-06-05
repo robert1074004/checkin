9a4390167fe0fbd0df589c46229d98d63c92041b
robert1074004 <abc50113@yahoo.com.tw>
2023-06-05 16:55:07 +0800

    feat: modify mysql connect setting

--- models/index.js
+++ models/index.js
@@ -7,7 +7,12 @@ const env = process.env.NODE_ENV || 'development'
 const config = require(path.resolve(__dirname, '../config/config.json'))[env]
 const db = {}
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config)
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  port: 3306,
+  dialect: 'mysql',
+  timeout: 5000
+})
 
 fs
   .readdirSync(__dirname)
